1f8e2854a2304d4971ca9c56cc19d33c3e284ea3
Darren Keen <darren.keen@fanduel.com>
2022-02-12 16:58:28 +0000

    add buy backs as ranked

--- src/constants.ts
+++ src/constants.ts
@@ -34,13 +34,17 @@ export const SEASON_START_END: Record<string, { start: number; end: number }> =
 export const MODE_KEYS: Record<string, string> = {
   br_brsolo: 'BR Solos',
   br_brz_brsolo: 'BR Solos',
+  br_vg_royale_solos: 'BR VG Solos',
   br_brduos: 'BR Duos',
   br_brz_brduos: 'BR Duos',
+  br_vg_royale_duos: 'BR VG Duos',
   br_brtrios: 'BR Trios',
   br_brz_brtrios: 'BR Trios',
+  br_vg_royale_trios: 'BR VG Trios',
   br_brquads: 'BR Quads',
   br_br_quads: 'BR Quads',
   br_brz_brquads: 'BR Quads',
+  br_vg_royale_quads: 'BR VG Quads',
   br_dbd_dbd: 'Iron Trial Trios',
   br_dbd_iron_trials_solos: 'Iron Trials Solos',
   br_dmz_plnbld: 'Blood Money',
@@ -54,6 +58,10 @@ export const MODE_KEYS: Record<string, string> = {
   br_kingslayer_rebirth_king_slayer: 'Kingslayer Rebirth',
   br_mini_miniroyale: 'Mini Royale',
   'br_rumble_lua_menu_mp/clash': 'Clash',
+  br_buy_back_solos: 'BR Buy Back Solos',
+  br_buy_back_duos: 'BR Buy Back Duos',
+  br_buy_back_trios: 'BR Buy Back Trios',
+  br_buy_back_quads: 'BR Buy Back Quads',
 };
 
 export const WITH_RANK_SOLO_MODE = ['br_brsolo', 'br_brz_brsolo'];
@@ -72,6 +80,14 @@ export const WITH_RANK_MODE = [
   'br_rebirth_rbrthquad',
   'br_mini_rebirth_mini_royale_duos',
   'br_mini_miniroyale',
+  'br_vg_royale_solos',
+  'br_vg_royale_duos',
+  'br_vg_royale_trios',
+  'br_vg_royale_quads',
+  'br_buy_back_solos',
+  'br_buy_back_duos',
+  'br_buy_back_trios',
+  'br_buy_back_quads',
 ];
 
 export const NON_SOLO_TROPHY_MODES = [
@@ -84,6 +100,14 @@ export const NON_SOLO_TROPHY_MODES = [
   'br_brz_brquads',
   'br_dbd_dbd',
   'br_dbd_iron_trials_solos',
+  'br_vg_royale_solos',
+  'br_vg_royale_duos',
+  'br_vg_royale_trios',
+  'br_vg_royale_quads',
+  'br_buy_back_solos',
+  'br_buy_back_duos',
+  'br_buy_back_trios',
+  'br_buy_back_quads',
 ];
 
 export const SOLO_TROPHY_MODES = ['br_brsolo', 'br_brz_brsolo'];
